ddb691be56b6ed4295cb01f3f7604d4c2f59784b
giomba <giomba@glgprograms.it>
2019-11-30 18:44:26 +0100

    [repo] removed Omnet settings from git tracking

--- src/out/clang-release/src/Receiver.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/Receiver.o: Receiver.cc Receiver.h \
-  C:/omnetpp-5.5.1/include\omnetpp.h
-
-Receiver.h:
-
-C:/omnetpp-5.5.1/include\omnetpp.h:
--- src/out/clang-release/src/Transmitter.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/Transmitter.o: Transmitter.cc Transmitter.h \
-  C:/omnetpp-5.5.1/include\omnetpp.h
-
-Transmitter.h:
-
-C:/omnetpp-5.5.1/include\omnetpp.h:
