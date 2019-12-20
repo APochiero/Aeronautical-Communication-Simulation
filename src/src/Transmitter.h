0df04a29698974c3b30ac3a1b3eed3061369a502
APochiero <32522627+APochiero@users.noreply.github.com>
2019-12-20 12:45:09 +0100

    Aggiunto penalty ad ogni controllo dell'handover

--- src/src/Transmitter.h
+++ src/src/Transmitter.h
@@ -47,6 +47,7 @@ class Transmitter : public cSimpleModule
     bool transmitting;
     bool penalty;
     bool schedulePenalty;
+    bool schedulePenaltyCheck;
     double arrivalTime;
     simtime_t handoverTime;
 
