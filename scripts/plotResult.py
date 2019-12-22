16d11695e0ea80c130314b2a624fc0ebcbba6673
APochiero <32522627+APochiero@users.noreply.github.com>
2019-12-22 10:43:13 +0100

    Aggiunti xticks label a plotResult.py

--- scripts/plotResult.py
+++ scripts/plotResult.py
@@ -2,19 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import math 
-import argparse 
+import math
+import argparse
 import seaborn as sns
 matplotlib.rcParams['font.family'] = "serif"
 
 t = [4.24, 7.42, 10.59, 13.77, 16.95]
 k = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2]
-colors = ['#944654','#08b2e3', '#9d8df1','#57a773','#484d6d']
+colors = ['#944654', '#08b2e3', '#9d8df1', '#57a773', '#484d6d']
 
 plt.style.use('ggplot')
 parser = argparse.ArgumentParser(description='Split Data by K')
-parser.add_argument('dirPath', help='path of directory containing Results files')
-parser.add_argument('distr', help='Distribution of Interarrival time [ exp | const ]')
+parser.add_argument(
+    'dirPath', help='path of directory containing Results files')
+parser.add_argument(
+    'distr', help='Distribution of Interarrival time [ exp | const ]')
 args = parser.parse_args()
 distribution = str(args.distr)
 path = str(args.dirPath)
@@ -36,31 +38,38 @@ for i in range(len(t)):
         waitingTimeCI = pd.to_numeric(df.iloc[8], errors='coerce')
         # Plot Delay
         plt.figure(1)
-        plt.errorbar(k, responseTime , yerr=responseTimeCI, fmt="--x", markeredgecolor='red', linewidth=0.8, capsize=4, label = 't= ' + str(t[i]))
+        plt.errorbar(k, responseTime, yerr=responseTimeCI, fmt="--x",
+                     markeredgecolor='red', linewidth=0.8, capsize=4, label='t= ' + str(t[i]))
         plt.xlabel('Interarrival Time [s]')
         plt.ylabel('Delay [s]')
         plt.ticklabel_format(axis='x', style='sci')
+        plt.xticks(np.arange(0.5, 2.25, step=0.25))
         plt.title(nameDistr + '\nEnd-to-End Delay')
         plt.legend()
         plt.grid(linestyle='--')
 
         # Plot Queue Length
         plt.figure(2)
-        plt.errorbar(k, queueLength, yerr=queueLengthCI, fmt="--x", markeredgecolor='red', linewidth=0.8, capsize=4, label = 't= ' + str(t[i]))
+        plt.errorbar(k, queueLength, yerr=queueLengthCI, fmt="--x",
+                     markeredgecolor='red', linewidth=0.8, capsize=4, label='t= ' + str(t[i]))
         plt.xlabel('Interarrival Time [s]')
         plt.ylabel('Queue Length')
         plt.ticklabel_format(axis='x', style='sci')
-        plt.title(nameDistr + '\nQueue Length Analysis') 
+        plt.xticks(np.arange(0.5, 2.25, step=0.25))
+        plt.title(nameDistr + '\nQueue Length Analysis')
         plt.legend()
         plt.grid(linestyle='--')
 
         # Bar plot Waiting Time Over Response Time
         plt.figure(3)
-        x = [ x - 0.04*(2-i) for x in k ]
-        plt.bar(x, responseTime, yerr=responseTimeCI, width=barWidth, error_kw=errorBarStyle, color='red' )
-        plt.bar(x, waitingTime, yerr=waitingTimeCI, width=barWidth, error_kw=errorBarStyle, label='t=' + str(t[i]), color = colors[i])
+        x = [x - 0.04*(2-i) for x in k]
+        plt.bar(x, responseTime, yerr=responseTimeCI,
+                width=barWidth, error_kw=errorBarStyle, color='red')
+        plt.bar(x, waitingTime, yerr=waitingTimeCI, width=barWidth,
+                error_kw=errorBarStyle, label='t=' + str(t[i]), color=colors[i])
         plt.xlabel('Interarrival Time [s]')
         plt.ylabel('Time [s]')
+        plt.xticks(np.arange(0.5, 2.25, step=0.25))
         plt.ticklabel_format(axis='x', style='sci')
         plt.title(nameDistr + '\nWaiting Time over Response Time')
         plt.legend()
